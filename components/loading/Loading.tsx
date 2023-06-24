6ee1c2f9235fab8ec439937fcc69edfea9a4b16e
murukal <tutu@fantufantu.com>
2023-06-24 23:28:12 +0800

    fix: loading element property error

--- components/loading/Loading.tsx
+++ components/loading/Loading.tsx
@@ -4,52 +4,52 @@ import clsx from "clsx";
 
 const Loading = () => {
   return (
-    <svg className="pl" width="240" height="240" viewBox="0 0 240 240">
+    <svg className={styles.loading} width="240" height="240" viewBox="0 0 240 240">
       <circle
-        className={clsx([styles["runner"], styles["circle-a"]])}
+        className={clsx([styles.runner, styles["circle-a"]])}
         cx="120"
         cy="120"
         r="105"
         fill="none"
         stroke="#000"
-        stroke-width="20"
-        stroke-dasharray="0 660"
-        stroke-dashoffset="-330"
-        stroke-linecap="round"
+        strokeWidth="20"
+        strokeDasharray="0 660"
+        strokeDashoffset="-330"
+        strokeLinecap="round"
       />
       <circle
-        className={clsx([styles["runner"], styles["circle-b"]])}
+        className={clsx([styles.runner, styles["circle-b"]])}
         cx="120"
         cy="120"
         r="35"
         fill="none"
         stroke="#000"
-        stroke-width="20"
-        stroke-dasharray="0 220"
-        stroke-dashoffset="-110"
-        stroke-linecap="round"
+        strokeWidth="20"
+        strokeDasharray="0 220"
+        strokeDashoffset="-110"
+        strokeLinecap="round"
       />
       <circle
-        className={clsx([styles["runner"], styles["circle-c"]])}
+        className={clsx([styles.runner, styles["circle-c"]])}
         cx="85"
         cy="120"
         r="70"
         fill="none"
         stroke="#000"
-        stroke-width="20"
-        stroke-dasharray="0 440"
-        stroke-linecap="round"
+        strokeWidth="20"
+        strokeDasharray="0 440"
+        strokeLinecap="round"
       />
       <circle
-        className={clsx([styles["runner"], styles["circle-d"]])}
+        className={clsx([styles.runner, styles["circle-d"]])}
         cx="155"
         cy="120"
         r="70"
         fill="none"
         stroke="#000"
-        stroke-width="20"
-        stroke-dasharray="0 440"
-        stroke-linecap="round"
+        strokeWidth="20"
+        strokeDashoffset="0 440"
+        strokeLinecap="round"
       />
     </svg>
   );
