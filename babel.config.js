db44162678f26987dec61657b2987cc80294e0a2
murukal <tutu@fantufantu.com>
2023-06-25 14:57:26 +0800

    feat: remove babel config because it used in config

--- babel.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('@babel/core').TransformOptions} */
-const configuration = {
-  presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
-};
-
-export default configuration;
