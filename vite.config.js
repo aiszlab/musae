b81d7d9bb824e103caaf9853a05748242eea9d09
murukal <tutu@fantufantu.com>
2023-06-29 00:44:00 +0800

    feat: use post css plugin

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import pkg from "./package.json" assert { type: "json" };
+import postcssNested from "postcss-nested";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    lib: {
-      entry: "./components/index.ts",
-      formats: ["es"],
-    },
     rollupOptions: {
+      input: "./components/index.ts",
       output: {
         preserveModules: true,
+        format: "es",
+        entryFileNames: "[name].js",
       },
+      preserveEntrySignatures: "exports-only",
       external: [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies)],
     },
     minify: false,
-    cssCodeSplit: true,
   },
   plugins: [
     react({
       jsxRuntime: "classic",
     }),
   ],
+
+  css: {
+    postcss: {
+      plugins: [postcssNested()],
+    },
+  },
 });
