c58543386af673877417ff6e3f1440d867e0ef7e
murukal <tutu@fantufantu.com>
2023-08-14 23:40:11 +0800

    feat: add switch component

--- components/index.ts
+++ components/index.ts
@@ -2,12 +2,13 @@
  * @description
  * components
  */
-export { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+export { Avatar } from "./avatar";
 export { Menu, type Props as MenuProps } from "./menu";
 export { Loading } from "./loading";
 export { Input } from "./input";
 export { Select } from "./select";
 export { Button } from "./button";
+export { Switch } from "./switch";
 
 /**
  * @description
---
+++ components/switch/switch.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Switch = () => {
+  return <input type="text" />;
+};
+
+export default Switch;
