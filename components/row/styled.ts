140652341969fb86b4c46a66cc0f986a7d2d412b
murukal <tutu@fantufantu.com>
2023-09-23 16:02:01 +0800

    chore: use ts declaration

--- components/row/styled.ts
+++ components/row/styled.ts
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { RowRenderProps } from "./types";
+import type { RowRenderProps } from "./types";
 
 export const StyledWrapper = styled.div<RowRenderProps>(({ gutters }) => {
   return {
