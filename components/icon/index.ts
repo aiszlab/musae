843a693ac10f8f131ae4848f003c0e1934787878
murukal <tutu@fantufantu.com>
2023-11-25 13:26:22 +0800

    feat: add icons export

--- components/icon/index.ts
+++ components/icon/index.ts
@@ -1,6 +1,10 @@
 import Icon from "./icon";
-import KeyboardDoubleArrowLeft from "./presets/keyboard-double-arrow-left";
-import KeyboardDoubleArrowRight from "./presets/keyboard-double-arrow-right";
+import KeyboardDoubleArrowLeft from "./icons/keyboard-double-arrow-left";
+import KeyboardDoubleArrowRight from "./icons/keyboard-double-arrow-right";
+import KeyboardArrowLeft from "./icons/keyboard-arrow-left";
+import KeyboardArrowRight from "./icons/keyboard-arrow-right";
+import KeyboardArrowUp from "./icons/keyboard-arrow-up";
+import KeyboardArrowDown from "./icons/keyboard-arrow-down";
 
 /**
  * @description
@@ -12,5 +16,11 @@ export { Icon };
  * @description
  * icon presets
  */
-export { KeyboardDoubleArrowLeft };
-export { KeyboardDoubleArrowRight };
+export {
+  KeyboardDoubleArrowLeft,
+  KeyboardDoubleArrowRight,
+  KeyboardArrowLeft,
+  KeyboardArrowRight,
+  KeyboardArrowUp,
+  KeyboardArrowDown,
+};
