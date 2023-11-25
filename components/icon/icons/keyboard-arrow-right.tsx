5e62b209762bc109ddca676afcf5609bd2b2e0d0
murukal <tutu@fantufantu.com>
2023-11-25 13:38:32 +0800

    fix: icon size and color

--- components/icon/icons/keyboard-arrow-right.tsx
+++ components/icon/icons/keyboard-arrow-right.tsx
@@ -3,8 +3,11 @@ import { withIcon } from "../hocs";
 
 const KeyboardArrowRight = withIcon((props) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-      <path d="M8.29498 16.59L12.875 12L8.29498 7.41L9.70498 6L15.705 12L9.70498 18L8.29498 16.59Z" fill="black" />
+    <svg xmlns="http://www.w3.org/2000/svg" width={props.size} height={props.size} viewBox="0 0 24 24">
+      <path
+        d="M8.29498 16.59L12.875 12L8.29498 7.41L9.70498 6L15.705 12L9.70498 18L8.29498 16.59Z"
+        fill={props.color}
+      />
     </svg>
   );
 });
