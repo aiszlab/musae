c52a8bf158b722fe815af9912666b50b103c72b6
murukal <tutu@fantufantu.com>
2024-03-13 08:35:56 +0800

    feat: add theme mode color mapping

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -63,56 +63,62 @@ export enum ColorToken {
  * convert palette to colors
  */
 export const toColors = (palette: Palette, mode: ContextValue["mode"]): Record<ColorToken, string> => {
+  const isLight = mode === "light";
+
   return {
-    [ColorToken.Primary]: mode === "light" ? palette.primary[40] : palette.primary[80],
-    [ColorToken.OnPrimary]: mode === "light" ? palette.primary[100] : palette.primary[20],
-    [ColorToken.PrimaryContainer]: palette.primary[90],
-    [ColorToken.OnPrimaryContainer]: palette.primary[10],
+    [ColorToken.Primary]: isLight ? palette.primary[40] : palette.primary[80],
+    [ColorToken.OnPrimary]: isLight ? palette.primary[100] : palette.primary[20],
+    [ColorToken.PrimaryContainer]: isLight ? palette.primary[90] : palette.primary[30],
+    [ColorToken.OnPrimaryContainer]: isLight ? palette.primary[10] : palette.primary[90],
+
     [ColorToken.PrimaryFixed]: palette.primary[90],
     [ColorToken.PrimaryFixedDim]: palette.primary[80],
     [ColorToken.OnPrimaryFixed]: palette.primary[10],
     [ColorToken.OnPrimaryFixedVariant]: palette.primary[30],
 
-    [ColorToken.Secondary]: palette.secondary[40],
-    [ColorToken.OnSecondary]: palette.secondary[100],
-    [ColorToken.SecondaryContainer]: palette.secondary[90],
-    [ColorToken.OnSecondaryContainer]: palette.secondary[10],
+    [ColorToken.Secondary]: isLight ? palette.secondary[40] : palette.secondary[80],
+    [ColorToken.OnSecondary]: isLight ? palette.secondary[100] : palette.secondary[20],
+    [ColorToken.SecondaryContainer]: isLight ? palette.secondary[90] : palette.secondary[30],
+    [ColorToken.OnSecondaryContainer]: isLight ? palette.secondary[10] : palette.secondary[90],
+
     [ColorToken.SecondaryFixed]: palette.secondary[90],
     [ColorToken.SecondaryFixedDim]: palette.secondary[80],
     [ColorToken.OnSecondaryFixed]: palette.secondary[10],
     [ColorToken.OnSecondaryFixedVariant]: palette.secondary[30],
 
-    [ColorToken.Tertiary]: palette.tertiary[40],
-    [ColorToken.OnTertiary]: palette.tertiary[100],
-    [ColorToken.TertiaryContainer]: palette.tertiary[90],
-    [ColorToken.OnTertiaryContainer]: palette.tertiary[10],
+    [ColorToken.Tertiary]: isLight ? palette.tertiary[40] : palette.tertiary[80],
+    [ColorToken.OnTertiary]: isLight ? palette.tertiary[100] : palette.tertiary[20],
+    [ColorToken.TertiaryContainer]: isLight ? palette.tertiary[90] : palette.tertiary[30],
+    [ColorToken.OnTertiaryContainer]: isLight ? palette.tertiary[10] : palette.tertiary[90],
+
     [ColorToken.TertiaryFixed]: palette.tertiary[90],
     [ColorToken.TertiaryFixedDim]: palette.tertiary[80],
     [ColorToken.OnTertiaryFixed]: palette.tertiary[10],
     [ColorToken.OnTertiaryFixedVariant]: palette.tertiary[30],
 
-    [ColorToken.Error]: palette.error[40],
-    [ColorToken.OnError]: palette.error[100],
-    [ColorToken.ErrorContainer]: palette.error[90],
-    [ColorToken.OnErrorContainer]: palette.error[10],
+    [ColorToken.Error]: isLight ? palette.error[40] : palette.error[80],
+    [ColorToken.OnError]: isLight ? palette.error[100] : palette.error[20],
+    [ColorToken.ErrorContainer]: isLight ? palette.error[90] : palette.error[30],
+    [ColorToken.OnErrorContainer]: isLight ? palette.error[10] : palette.error[90],
 
-    [ColorToken.Surface]: mode === "light" ? palette.neutral[99] : palette.neutral[10],
-    [ColorToken.OnSurface]: mode === "light" ? palette.neutral[10] : palette.neutral[90],
-    [ColorToken.SurfaceDim]: mode === "light" ? palette.neutral[90] : palette.neutral[10],
+    [ColorToken.Surface]: isLight ? palette.neutral[99] : palette.neutral[10],
+    [ColorToken.SurfaceDim]: isLight ? palette.neutral[90] : palette.neutral[10],
 
-    [ColorToken.SurfaceContainerLowest]: mode === "light" ? palette.neutral[100] : palette.neutral[0],
-    [ColorToken.SurfaceContainerLow]: mode === "light" ? palette.neutral[95] : palette.neutral[10],
-    [ColorToken.SurfaceContainer]: mode === "light" ? palette.neutral[95] : palette.neutral[10],
-    [ColorToken.SurfaceContainerHigh]: mode === "light" ? palette.neutral[95] : palette.neutral[10],
-    [ColorToken.SurfaceContainerHighest]: mode === "light" ? palette.neutral[90] : palette.neutral[20],
+    [ColorToken.SurfaceContainerLowest]: isLight ? palette.neutral[100] : palette.neutral[0],
+    [ColorToken.SurfaceContainerLow]: isLight ? palette.neutral[95] : palette.neutral[10],
+    [ColorToken.SurfaceContainer]: isLight ? palette.neutral[95] : palette.neutral[10],
+    [ColorToken.SurfaceContainerHigh]: isLight ? palette.neutral[95] : palette.neutral[10],
+    [ColorToken.SurfaceContainerHighest]: isLight ? palette.neutral[90] : palette.neutral[20],
 
-    [ColorToken.OnSurfaceVariant]: palette.neutralVariant[30],
-    [ColorToken.InverseSurface]: palette.neutral[20],
-    [ColorToken.InverseOnSurface]: palette.neutral[95],
-    [ColorToken.InversePrimary]: palette.primary[80],
+    [ColorToken.OnSurface]: isLight ? palette.neutral[10] : palette.neutral[90],
+    [ColorToken.OnSurfaceVariant]: isLight ? palette.neutralVariant[30] : palette.neutralVariant[80],
 
-    [ColorToken.Outline]: palette.neutralVariant[50],
-    [ColorToken.OutlineVariant]: palette.neutralVariant[80],
+    [ColorToken.InverseSurface]: isLight ? palette.neutral[20] : palette.neutral[90],
+    [ColorToken.InverseOnSurface]: isLight ? palette.neutral[95] : palette.neutral[20],
+    [ColorToken.InversePrimary]: isLight ? palette.primary[80] : palette.primary[40],
+
+    [ColorToken.Outline]: isLight ? palette.neutralVariant[50] : palette.neutralVariant[60],
+    [ColorToken.OutlineVariant]: isLight ? palette.neutralVariant[80] : palette.neutralVariant[30],
 
     [ColorToken.Shadow]: palette.neutral[0],
     [ColorToken.Scrim]: palette.neutral[0],
