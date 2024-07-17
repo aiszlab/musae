272737cc40810d4893f12d6d2640222fc0d4b3ff
murukal <tutu@fantufantu.com>
2024-07-17 09:16:03 +0800

    feat: improve badge styles

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { useClassNames } from "../../hooks/use-class-names";
 import { BadgeClassToken, ComponentToken } from "../../utils/class-name";
 import { BadgeProps } from "./types";
 import stylex from "@stylexjs/stylex";
 import clsx from "clsx";
+import { typography } from "../theme/theme";
+import { sizes } from "../theme/tokens.stylex";
+import { useTheme } from "../theme";
+import { ColorToken } from "../../utils/colors";
 
 const styles = stylex.create({
   badge: {
@@ -11,20 +15,33 @@ const styles = stylex.create({
     display: "inline-flex",
   },
 
-  tail: {
+  tail: (props: { backgroundColor: CSSProperties["backgroundColor"]; color: CSSProperties["color"] }) => ({
     position: "absolute",
     top: 0,
     right: 0,
     transform: "translateX(50%) translateY(-50%)",
-  },
+    borderRadius: sizes.infinity,
+    minWidth: sizes.xxxsmall,
+    textAlign: "center",
+
+    backgroundColor: props.backgroundColor,
+    color: props.color,
+  }),
 });
 
 const Badge = ({ className, style, children, content }: BadgeProps) => {
   const classNames = useClassNames(ComponentToken.Badge);
+  const theme = useTheme();
 
   const styled = {
     badge: stylex.props(styles.badge),
-    tail: stylex.props(styles.tail),
+    tail: stylex.props(
+      styles.tail({
+        backgroundColor: theme.colors[ColorToken.Primary],
+        color: theme.colors[ColorToken.OnPrimary],
+      }),
+      typography.label.small,
+    ),
   };
 
   return (
