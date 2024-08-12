047d8f4247b003d169eb77bd9e7ab2a5dae83ba9
murukal <tutu@fantufantu.com>
2024-08-12 13:57:33 +0800

    chore: add Breadcrumb unit test

--- src/components/breadcrumb/__test__/index.test.tsx
+++ src/components/breadcrumb/__test__/index.test.tsx
@@ -5,28 +5,42 @@ import "@testing-library/jest-dom";
 
 describe("`Breadcrumb` Component", () => {
   test("empty items", () => {
-    const { container } = render(<Breadcrumb items={[]} />);
+    const { container, asFragment } = render(<Breadcrumb />);
+
     expect(container.hasChildNodes()).toBeFalsy();
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test("display items", () => {
-    const { container } = render(
-      <Breadcrumb items={[{ label: "label1" }, { label: "link2", href: "https://aiszlab.dev" }, { label: "label3" }]} />
+    const { container, asFragment } = render(
+      <Breadcrumb
+        items={[
+          { label: "label1" },
+          { label: "link2", href: "https://aiszlab.dev" },
+          { label: "label3" },
+        ]}
+      />,
     );
 
     expect(container.hasChildNodes()).toBeTruthy();
     expect(container.querySelectorAll(".musae-breadcrumb__item").length).toBe(3);
     expect(container.querySelectorAll(".musae-breadcrumb__separator").length).toBe(2);
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test("customize separator", () => {
-    const { getAllByRole } = render(
+    const { getAllByRole, asFragment } = render(
       <Breadcrumb
-        items={[{ label: "label1" }, { label: "link2", href: "https://aiszlab.dev" }, { label: "label3" }]}
+        items={[
+          { label: "label1" },
+          { label: "link2", href: "https://aiszlab.dev" },
+          { label: "label3" },
+        ]}
         separator="-"
-      />
+      />,
     );
 
     expect(getAllByRole("separator").filter((item) => item.innerHTML === "-").length).toBe(2);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
