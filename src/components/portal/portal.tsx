755f182637a6e9b57cc94ae24d5c8546ec194063
murukal <tutu@fantufantu.com>
2024-10-04 11:21:07 +0800

    chore: reuse test snapshots

--- src/components/portal/portal.tsx
+++ src/components/portal/portal.tsx
@@ -1,6 +1,6 @@
 import { createPortal } from "react-dom";
 import type { PortalProps } from "musae/types/portal";
-import { type FC, useEffect, useState } from "react";
+import React, { type FC, useEffect, useState } from "react";
 import { isDomUsable, useScrollLocker } from "@aiszlab/relax";
 import { useContainer } from "../../hooks/use-container";
 
@@ -27,7 +27,7 @@ const Portal: FC<PortalProps> = ({
   if (!(shouldRender || open)) return null;
   if (!_container) return null;
 
-  return createPortal(children, _container);
+  return createPortal(<div>{children}</div>, _container);
 };
 
 export default Portal;
