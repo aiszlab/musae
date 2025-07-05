30aa73d15a620cc968f1c0e32588f08a55f16de8
murukal <tutu@fantufantu.com>
2025-07-05 19:50:48 +0800

    chore: update version to 1.0.7-beta.19

--- src/components/form/list.tsx
+++ src/components/form/list.tsx
@@ -1,6 +1,16 @@
-import React, { createContext, ReactNode, useContext, useMemo, useState } from "react";
+import React, {
+  createContext,
+  forwardRef,
+  ReactNode,
+  useCallback,
+  useContext,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import Item from "./item";
-import type { FieldsValue, FormListProps } from "../../types/form";
+import type { FieldsValue, FormListProps, UsingForm } from "../../types/form";
 import { useEvent, useIdentity, replaceAt, at } from "@aiszlab/relax";
 import { FormContext } from "./context";
 import { useForm } from "./hooks";
@@ -10,6 +20,10 @@ interface ContextValue<V extends FieldsValue> {
   onChange?: (field: string, value: V) => void;
   values?: V[];
   fields: string[];
+}
+
+interface ListRef {
+  remove: (field: string) => void;
 }
 
 /**
@@ -31,12 +45,18 @@ function _Item({ field, children }: { field: string; children: ReactNode }) {
     return at(values, fields.indexOf(field));
   }, [values, fields, field]);
 
-  // create form instance
-  const _form = useForm<{}>({
-    value,
-    onChange: (_names, value) => {
+  // value change handler
+  const changeFieldValue = useCallback<Required<UsingForm<FieldsValue>>["onChange"]>(
+    (_names, value) => {
       onChange?.(field, value);
     },
+    [field],
+  );
+
+  // create form instance
+  const _form = useForm<FieldsValue>({
+    value,
+    onChange: changeFieldValue,
   });
 
   return (
@@ -47,35 +67,41 @@ function _Item({ field, children }: { field: string; children: ReactNode }) {
 /**
  * internal used Component
  */
-function _List<V extends FieldsValue>({
-  values = [],
-  onChange,
-  children,
-  fields,
-}: {
-  fields: string[];
-  values?: V[];
-  onChange?: (values: V[]) => void;
-  children: ReactNode;
-}) {
-  const changeItemValue = (field: string, value: V) => {
-    const _index = fields.indexOf(field);
-    onChange?.(replaceAt(values, _index, value));
+const _List = forwardRef<
+  ListRef,
+  {
+    fields: string[];
+    values?: FieldsValue[];
+    onChange?: (values: FieldsValue[]) => void;
+    children: ReactNode;
+  }
+>(function ({ values = [], onChange, children, fields }, ref) {
+  const changeItemValue = (field: string, value: FieldsValue) => {
+    const _at = fields.indexOf(field);
+    onChange?.(replaceAt(values, _at, value));
   };
+
+  useImperativeHandle(ref, () => {
+    return {
+      remove: (field: string) => {
+        const _at = fields.indexOf(field);
+        onChange?.(values.toSpliced(_at, 1));
+      },
+    };
+  });
 
   return (
     <Context.Provider
       value={{
         fields,
         values,
-        // @ts-expect-error 合理的泛型设计，在实际使用中，表单属性会被扩充
         onChange: changeItemValue,
       }}
     >
       {children}
     </Context.Provider>
   );
-}
+});
 
 /**
  * wrapped `Form.List` Component
@@ -87,6 +113,7 @@ function List<T extends FieldsValue, FieldKey extends keyof T>({
   const { 1: _id } = useIdentity();
   const [_fields, setFields] = useState<Set<string>>(new Set());
   const fields = useMemo(() => Array.from(_fields), [_fields]);
+  const listRef = useRef<ListRef>(null);
 
   const add = useEvent(() => {
     setFields((prev) => new Set(prev).add(_id()));
@@ -98,6 +125,7 @@ function List<T extends FieldsValue, FieldKey extends keyof T>({
       next.delete(field);
       return next;
     });
+    listRef.current?.remove(field);
   });
 
   // no render, return null
@@ -105,7 +133,9 @@ function List<T extends FieldsValue, FieldKey extends keyof T>({
 
   return (
     <Item {...itemProps}>
-      <_List fields={fields}>{children({ fields, add, remove })}</_List>
+      <_List ref={listRef} fields={fields}>
+        {children({ fields, add, remove })}
+      </_List>
     </Item>
   );
 }
