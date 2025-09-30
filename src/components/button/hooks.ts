c51d66bf737af8352a7d5035fdf931856e5d5184
murukal <tutu@fantufantu.com>
2025-09-30 09:35:28 +0800

    feat(组件): 添加 loading 状态支持到 Button 组件

--- src/components/button/hooks.ts
+++ src/components/button/hooks.ts
@@ -1,23 +1,29 @@
 import { type MouseEvent } from "react";
 import { useRipple } from "../ripple";
 import type { ButtonProps } from "../../types/button";
-import { useEvent } from "@aiszlab/relax";
+import { useControlledState, useEvent } from "@aiszlab/relax";
 
 /**
  * @description
  * hooks for button component
  */
-export const useButton = ({ onClick: click }: Pick<ButtonProps, "onClick">) => {
+export const useButton = ({ onClick, loading }: Pick<ButtonProps, "onClick" | "loading">) => {
+  const [isLoading, setIsLoading] = useControlledState(loading);
   const { ripples, add, clear } = useRipple();
 
-  const onClick = useEvent((event: MouseEvent<HTMLButtonElement>) => {
-    click?.(event);
+  const click = useEvent(async (event: MouseEvent<HTMLButtonElement>) => {
     add(event);
+
+    setIsLoading(true);
+    await Promise.try(() => onClick?.(event)).finally(() => {
+      setIsLoading(false);
+    });
   });
 
   return {
     ripples,
-    onClick,
+    click,
     clear,
+    isLoading,
   };
 };
