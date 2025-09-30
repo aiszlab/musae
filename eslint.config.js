0b773a1e9a055ae0af01a5b15d7f7748e74133a9
murukal <tutu@fantufantu.com>
2025-09-30 23:47:51 +0800

    ``` feat(storybook): 配置 storybook 并添加相关依赖
    - 添加 .storybook/main.ts 和 .storybook/preview.ts 配置文件
    - 更新 .gitignore 忽略 storybook-static 目录
    - 移除 package.json 中 rollup 构建步骤，简化 storybook 启动和构建命令
    - 安装 storybook 相关依赖包，包括 react-vite 框架支持和文档插件
    - 更新 eslint 配置注释，为后续启用 storybook 插做准备
    ```

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+// import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
